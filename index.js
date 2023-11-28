dfce320a121a773c14316e80eb4177421e0188ae
Letícia Lima Silva <leticiaakiralele@gmail.com>
2023-11-28 01:53:37 -0300

    Salvando uma tarefa no banco de dados

--- index.js
+++ index.js
@@ -9,6 +9,31 @@ app.set('viem engine', 'handlebars')
 
 app.use(express.static('public'))
 
+app.use(express.urlencoded({
+    extended: true
+}))
+
+app.use(express.json())
+
+
+app.post('/criar', (requisicao, resposta) => {
+    const descricao = requisição.body.descricao
+    const completa = 0
+
+    const sql = `
+        INSERT INTO tarefas(descricao, completa)
+        VALUES ('${descricao}', '${completa}')
+    `
+
+    conexao.query(sql, (erro) => {
+        if (erro) {
+            return console.log(erro)
+        }
+
+        resposta.redirect('/')
+    })
+})
+
 app.get('/', (requisicao, resposta) => {
     resposta.render('home.handlebars')
 })
